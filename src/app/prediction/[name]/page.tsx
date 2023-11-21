34c31694a44247e669162f52e29b38e11c2445ac
kadunapikin <ibakoshi27@gmail.com>
2023-11-22 00:05:43 +0100

    create a getPredictedAge function to get fetch users age

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,3 +1,9 @@
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`)
+  return res.json();
+}
+
+
 interface Params {
   params: {name: string}
 }
