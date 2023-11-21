e66a3855328070db89efaff17cb78419723011c7
kadunapikin <ibakoshi27@gmail.com>
2023-11-21 18:10:25 +0100

    add an onchange function to input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+import { useState } from "react";
+
 export default function Home() {
+  const [inputValue, SetInputValue] = useState('');
+
   return (
     <div>
       <div>
         <h1>Enter Your Name</h1>
       </div>
       <form action="">
-        <input type="text" placeholder="Type Your Name" />
+        <input type="text" placeholder="Type Your Name" value={inputValue} onChange={(e) => SetInputValue(e.target.value)}/>
         <button type="submit">Predict Data</button>
       </form>
     </div>
