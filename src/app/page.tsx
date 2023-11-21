07e026a319172eaf493a4d32c977792ca549d634
kadunapikin <ibakoshi27@gmail.com>
2023-11-21 17:56:51 +0100

    add input form and submit button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div>
+        <h1>Enter Your Name</h1>
+      </div>
+      <form action="">
+        <input type="text" placeholder="Type Your Name" />
+        <button type="submit">Predict Data</button>
+      </form>
     </div>
   )
 }
